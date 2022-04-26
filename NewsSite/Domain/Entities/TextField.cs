735f82279092fb24050369efcc2fac0b707d006a
Anatoly <gymbotan@rambler.ru>
2022-04-26 13:50:31 +0300

    feat: Add Services controller and Show, Index views to see one or all news

--- NewsSite/Domain/Entities/TextField.cs
+++ NewsSite/Domain/Entities/TextField.cs
@@ -12,9 +12,9 @@ namespace NewsSite.Domain.Entities
         public string CodeWord { get; set; }
 
         [Display(Name = "Название страницы (заголовок)")]
-        public override string Title { get; set; }
+        public override string Title { get; set; } = "Информационная страница";
 
         [Display(Name = "Содержание страницы")]
-        public override string Text { get; set; }
+        public override string Text { get; set; } = "Содержание заполняется администратором";
     }
 }
